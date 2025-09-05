a138c5e6d770677ff1a3ceed599de39c02ed3931
Arsh-kb <kaurarshvir9@gmail.com>
2025-09-05 11:32:06 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -13,16 +13,16 @@ import {
   signInAnonymously,
 } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 
-// 🔑 Full Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyAkS6uwNFtT32WztLije-uErDnvucKcmzQ",
-  authDomain: "shopping-cart-d2963.firebaseapp.com",
-  databaseURL: "https://shopping-cart-d2963-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "shopping-cart-d2963",
-  storageBucket: "shopping-cart-d2963.firebasestorage.app",
-  messagingSenderId: "464387276253",
-  appId: "1:464387276253:web:57fc2ce35f20086fe8ca34"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -136,3 +136,4 @@ const appendList = (inputValue, itemId, shoppingListInDB) => {
 
   listItem.appendChild(li);
 };
+
